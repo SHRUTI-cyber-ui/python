1c53e47292d821194e8d26e83456941d74e7c023
SHRUTI-cyber-ui <sp4124476@gmail.com>
2024-08-31 20:16:02 +0530

    Add files via upload

---
+++ classsize.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class {                  //class name s, jis class ka name na ho use ananous kahte hai
+//	int d ,b;  //bool
+//	bool ss;
+	
+}s;
+int main()
+{
+     cout<<"size of s"<<sizeof(s);
+}
---
+++ condition.cpp
@@ -0,0 +1,17 @@
+//doubt class 2 and 3 and not divi. by 5
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=10; i<=50; i++)
+    {
+        if(i%2==0 and i%3==0)
+        {
+            if(i%5!=0)
+            {
+                cout<<i<<endl;
+            }                                          
+        }
+    }
+    
+}
